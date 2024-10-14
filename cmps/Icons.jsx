49180683b26ac0f2be02962f255338e35d651c33
Amit Baruch <amitbaruch08@gmail.com>
2024-10-14 11:56:37 +0300

    Add some icons for home page

---
+++ cmps/Icons.jsx
@@ -0,0 +1,59 @@
+export function Icons() {
+    return (
+        <React.Fragment>
+            {/* Gmail Icon */}
+            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 48 48">
+                <path fill="#4caf50" d="M45,16.2l-5,2.75l-5,4.75L35,40h7c1.657,0,3-1.343,3-3V16.2z"></path>
+                <path fill="#1e88e5" d="M3,16.2l3.614,1.71L13,23.7V40H6c-1.657,0-3-1.343-3-3V16.2z"></path>
+                <polygon
+                    fill="#e53935"
+                    points="35,11.2 24,19.45 13,11.2 12,17 13,23.7 24,31.95 35,23.7 36,17"
+                ></polygon>
+                <path
+                    fill="#c62828"
+                    d="M3,12.298V16.2l10,7.5V11.2L9.876,8.859C9.132,8.301,8.228,8,7.298,8h0C4.924,8,3,9.924,3,12.298z"
+                ></path>
+                <path
+                    fill="#fbc02d"
+                    d="M45,12.298V16.2l-10,7.5V11.2l3.124-2.341C38.868,8.301,39.772,8,40.702,8h0 C43.076,8,45,9.924,45,12.298z"
+                ></path>
+            </svg>
+
+            {/* Google Keep Icon */}
+            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 48 48">
+                <linearGradient
+                    id="kM1uKwC9sUKGexMTNJd_qa_SEHsYigx9DvU_gr1"
+                    x1="21.854"
+                    x2="25.223"
+                    y1="4.726"
+                    y2="41.333"
+                    gradientUnits="userSpaceOnUse"
+                >
+                    <stop offset="0" stop-color="#ffc153"></stop>
+                    <stop offset="1" stop-color="#f0a737"></stop>
+                </linearGradient>
+                <path
+                    fill="url(#kM1uKwC9sUKGexMTNJd_qa_SEHsYigx9DvU_gr1)"
+                    d="M38,15v26c0,1.105-0.895,2-2,2H12c-1.105,0-2-0.895-2-2V7c0-1.105,0.895-2,2-2h16L38,15z"
+                ></path>
+                <linearGradient
+                    id="kM1uKwC9sUKGexMTNJd_qb_SEHsYigx9DvU_gr2"
+                    x1="33.102"
+                    x2="32.952"
+                    y1=".68"
+                    y2="14.433"
+                    gradientUnits="userSpaceOnUse"
+                >
+                    <stop offset="0" stop-color="#ffa732"></stop>
+                    <stop offset="1" stop-color="#ff9032"></stop>
+                </linearGradient>
+                <path fill="url(#kM1uKwC9sUKGexMTNJd_qb_SEHsYigx9DvU_gr2)" d="M28,5v10h10L28,5z"></path>
+                <path
+                    fill="#fff"
+                    d="M20.686,29h6.628C28.932,27.925,30,26.088,30,24c0-3.314-2.686-6-6-6s-6,2.686-6,6 C18,26.088,19.068,27.925,20.686,29z"
+                ></path>
+                <rect width="6" height="2" x="21" y="32" fill="#fff"></rect>
+            </svg>
+        </React.Fragment>
+    )
+}
--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,6 +1,10 @@
+import { Icons } from '../cmps/Icons.jsx'
 
 export function Home() {
-    return <section className="home">
-        <h1>Welcome to home page!</h1>
-    </section>
+    return (
+        <section className="home">
+            <h1>Welcome to home page!</h1>
+            <Icons />
+        </section>
+    )
 }
