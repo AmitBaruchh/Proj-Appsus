b44179786e5ea6d6c19f3fc8c16d59bf452cb417
Avi Museri <avimus@my.hit.ac.il>
2024-10-14 15:59:11 +0300

    add MailIndex component

--- apps/mail/pages/MailIndex.jsx
+++ apps/mail/pages/MailIndex.jsx
@@ -1,5 +1,49 @@
+const { useEffect, useState } = React
+
+import { MailList } from "../cmps/MailList.jsx"
+import { mailService } from "../services/mail.service.js"
 
 export function MailIndex() {
-    return <div>mail app</div>
+
+    const [emails, setEmails] = useState(null)
+
+    useEffect(() => {
+        console.log('effect')
+        loadMails()
+    }, [emails])
+
+    function loadMails() {
+        // console.log('loadMails..')
+        mailService.query()
+            .then(setEmails)
+            .catch(err => {
+                console.log('problems getting mails:', err)
+            })
+    }
+
+    function onRemoveMail(mailId) {
+        mailService.remove(mailId)
+            .then(() => {
+                setCars(mails => mails.filter(mail => mail.id !== mailId))
+            })
+            .catch(err => {
+                console.log('Problems removing mail:', err)
+            })
+    }
+
+    // function onSetFilterBy(filterBy) {
+    //     setFilterBy(preFilter => ({ ...preFilter, ...filterBy }))
+    // }
+
+
+    if (!emails) return <div>Loading...</div>
+    return (<section className="mail-index">
+
+        <MailList
+            emails={emails}
+            onRemoveMail={onRemoveMail}
+        />
+
+    </section>)
 }
 
