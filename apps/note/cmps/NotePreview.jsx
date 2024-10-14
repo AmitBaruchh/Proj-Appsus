b1f06e9004026483764760eca46f77911719ef62
Amit Baruch <amitbaruch08@gmail.com>
2024-10-14 17:53:14 +0300

    Refactor notes to use dynamic components

--- apps/note/cmps/NotePreview.jsx
+++ apps/note/cmps/NotePreview.jsx
@@ -1,31 +1,44 @@
 export function NotePreview({ note }) {
-    const { type, info } = note
-
     return (
         <article className="note-preview">
-            {type === 'NoteTxt' && <p>{info.txt}</p>}
-            {type === 'NoteImg' && (
-                <div>
-                    <p>{info.title}</p>
-                    <img src={info.url} alt={info.title} />
-                </div>
-            )}
-            {type === 'NoteVideo' && (
-                <div>
-                    <p>{info.title}</p>
-                    <iframe src={info.url} width="100%" height="300"></iframe>
-                </div>
-            )}
-            {type === 'NoteTodos' && (
-                <div>
-                    <h3>{info.title}</h3>
-                    <ul>
-                        {info.todos.map((todo, idx) => (
-                            <li key={idx}>{todo.txt}</li>
-                        ))}
-                    </ul>
-                </div>
-            )}
+            <DynamicCmp note={note} />
         </article>
+    )
+}
+
+function DynamicCmp(props) {
+    switch (props.note.type) {
+        case 'NoteTxt':
+            return <NoteTxt {...props} />
+        case 'NoteImg':
+            return <NoteImg {...props} />
+        case 'NoteTodos':
+            return <NoteTodos {...props} />
+    }
+}
+
+function NoteTxt({ note }) {
+    return <p>{note.info.txt}</p>
+}
+
+function NoteImg({ note }) {
+    return (
+        <div>
+            <p>{note.info.title}</p>
+            <img src={note.info.url} alt={note.info.title} />
+        </div>
+    )
+}
+
+function NoteTodos({ note }) {
+    return (
+        <div>
+            <h3>{note.info.title}</h3>
+            <ul>
+                {note.info.todos.map((todo, idx) => (
+                    <li key={idx}>{todo.txt}</li>
+                ))}
+            </ul>
+        </div>
     )
 }
