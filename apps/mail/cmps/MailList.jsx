68496383916b98609506070ee5176037aede5203
Avi Museri <avimus@my.hit.ac.il>
2024-10-14 16:00:41 +0300

    Add MailList component

--- apps/mail/cmps/MailList.jsx
+++ apps/mail/cmps/MailList.jsx
@@ -1,3 +1,17 @@
-export function MailList() {
-    return <div>Mail list</div>
+import { MailPreview } from "./MailPreview.jsx";
+
+export function MailList({ emails, onRemoveMail }) {
+
+    return (
+        <ul className="mail-list">
+            {emails.map(mail =>
+                <li key={mail.id}>
+                    <MailPreview mail={mail} />
+                    <section>
+                        <button onClick={() => onRemoveMail(mail.id)}>Remove</button>
+                    </section>
+                </li>
+            )}
+        </ul>
+    )
 }
