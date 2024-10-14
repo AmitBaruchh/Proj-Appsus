a97b27ee932bf693c6ecedeef2dae71758958f5c
Avi Museri <avimus@my.hit.ac.il>
2024-10-14 21:05:11 +0300

    add format formatTime function to service.

--- apps/mail/services/mail.service.js
+++ apps/mail/services/mail.service.js
@@ -17,7 +17,8 @@ export const mailService = {
     remove,
     save,
     getEmptyMail,
-    getDefaultFilter
+    getDefaultFilter,
+    formatTime
 }
 
 function query(filterBy = {}) {
@@ -41,8 +42,18 @@ function save(mail) {
     }
 }
 
-function getEmptyMail(subject = 'subject', isRead = false) {
-    return { subject, isRead }
+function getEmptyMail(subject = 'subject', isRead) {
+
+    return {
+        createdAt: Date.now(),
+        subject: subject || 'subject',
+        body: 'Would love to catch up sometimes',
+        isRead: isRead || false,
+        sentAt: Date.now() + 30,
+        removedAt: null,
+        from: 'momo@momo.com',
+        to: 'user@appsus.com'
+    }
 }
 
 function getDefaultFilter() {
@@ -56,9 +67,9 @@ function _createMails() {
     let mails = loadFromStorage(MAIL_KEY)
     if (!mails || !mails.length) {
         mails = [
-            _createMail('New jobs posted from careers.sapiens.com'),
+            _createMail('New jobs posted from careers.sapiens.com', true),
             _createMail(`Last week's updates in your shared folders`),
-            _createMail('Dana reacted to a new post'),
+            _createMail('Dana reacted to a new post', true),
             _createMail('This is why you’re not landing interviews')
         ]
         saveToStorage(MAIL_KEY, mails)
@@ -66,16 +77,30 @@ function _createMails() {
 }
 
 function _createMail(subject, isRead) {
-    const mail = {
-        id: makeId(),
-        createdAt: Date.now(),
-        subject: subject || 'subject',
-        body: 'Would love to catch up sometimes',
-        isRead: false,
-        sentAt: Date.now() + 30,
-        removedAt: null,
-        from: 'momo@momo.com',
-        to: 'user@appsus.com'
-    }
+
+    const mail = getEmptyMail(subject, isRead)
+    mail.id = makeId()
     return mail
+}
+
+function formatTime(timestamp) {
+    const currentTime = Date.now();
+    const timeDifference = currentTime - timestamp;
+
+    const oneDayInMilliseconds = 24 * 60 * 60 * 1000 // 24 hours in milliseconds
+
+    // If the time difference is more than 24 hours, return the date (in "month day" format)
+    if (timeDifference > oneDayInMilliseconds) {
+        const date = new Date(timestamp)
+        const options = { month: 'short', day: 'numeric' }
+        return date.toLocaleDateString('en-US', options)
+    } else {
+        // If within 24 hours, return the hours in "hh:mm AM/PM" format
+        const date = new Date(timestamp);
+        let hours = date.getHours();
+        const minutes = date.getMinutes().toString().padStart(2, '0');
+        const ampm = hours >= 12 ? 'PM' : 'AM';
+        hours = hours % 12 || 12; // Convert to 12-hour format
+        return `${hours}:${minutes} ${ampm}`;
+    }
 }
