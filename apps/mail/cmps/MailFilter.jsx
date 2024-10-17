210995129943629c3304d8f047d1e7ecac32e3ec
Avi Museri <avimus@my.hit.ac.il>
2024-10-17 14:56:34 +0300

    fix material-symbols-outlined bug

--- apps/mail/cmps/MailFilter.jsx
+++ apps/mail/cmps/MailFilter.jsx
@@ -1,7 +1,7 @@
 export function MailFilter() {
     return (
         <section className="mail-filter">
-            <span class="material-symbols-outlined">search</span>
+            <span class="material-symbols-outlined search">search</span>
             <input className='mail-input' type="text" placeholder="Search mail" />
             <span class="material-symbols-outlined">tune</span>
         </section>
