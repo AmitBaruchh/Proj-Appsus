1d784a58d141916ad398535a080f97a926007661
Avi Museri <avimus@my.hit.ac.il>
2024-10-17 14:59:01 +0300

    add icon style

--- apps/mail/cmps/MailFilter.jsx
+++ apps/mail/cmps/MailFilter.jsx
@@ -1,9 +1,9 @@
 export function MailFilter() {
     return (
         <section className="mail-filter">
-            <span class="material-symbols-outlined search">search</span>
+            <span class="material-symbols-outlined icon">search</span>
             <input className='mail-input' type="text" placeholder="Search mail" />
-            <span class="material-symbols-outlined">tune</span>
+            <span class="material-symbols-outlined icon">tune</span>
         </section>
     )
 }
