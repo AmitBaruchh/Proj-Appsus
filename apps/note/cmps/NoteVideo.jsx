401f199f03f79cc7deae5698ecdc3c5e0991126b
Amit Baruch <amitbaruch08@gmail.com>
2024-10-18 16:44:30 +0300

    Improve notes layout

--- apps/note/cmps/NoteVideo.jsx
+++ apps/note/cmps/NoteVideo.jsx
@@ -2,7 +2,7 @@ export function NoteVideo({ note }) {
     return (
         <div>
             {note.info.title && <h3>{note.info.title}</h3>}
-            <video width="240" height="160ש" controls>
+            <video width="210" height="130" controls>
                 <source src={note.info.url} type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
