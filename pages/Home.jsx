aa0ac9e560e021ac49598a6ea50fe401fc282a89
Avi Museri <avimus@my.hit.ac.il>
2024-10-19 22:35:05 +0300

    Add style to home page

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -3,8 +3,14 @@ import { Icons } from '../cmps/Icons.jsx'
 export function Home() {
     return (
         <section className="home">
-            <h1>Welcome to home page!</h1>
+            <h1 className='main-header'>Welcome to Appsus App!</h1>
+            <p className='description'>
+                Our app offers a solution by combining your emails and notes into one efficient platform.<br />
+                Manage your inbox, get ideas and stay organized without juggling multiple apps.<br />
+                With smart tools and real-time sync, it's the perfect place to stay connected and turn your thoughts into action without effort.
+            </p>
             <Icons />
+
         </section>
     )
 }
